68ad0536a800da970af6871b80e72e59a847f251
almartin82 <almartin@gmail.com>
2015-01-26 11:51:08 -0500

    scaffold with start end rit and other headline cdf fields

--- R/growth_df_prep.R
+++ R/growth_df_prep.R
@@ -10,8 +10,6 @@
 #' valid 'growth_window', such as 'Fall to Spring'. 
 #' 
 #' @param processed_cdf a conforming processed_cdf data frame
-#' @param start_season the start of the growth window ("Fall", "Winter", or "Spring")
-#' @param end_season the end of the growth window ("Fall", "Winter", or "Spring")
 #' @param norm_df defaults to norms_students_2011.  if you have a conforming norms object,
 #' you can use generate_growth_df to produce a growth data frame for those norms.
 #' example usage: calculate college ready growth norms, and use generate_growth_df to see
@@ -25,10 +23,8 @@
 #' 
 #' @export
 
-generate_growth_df <- function(
+generate_growth_dfs <- function(
   processed_cdf,
-  start_season, 
-  end_season,
   norm_df=norms_students_2011,
   include_unsanctioned_windows=FALSE
 ){  
@@ -38,19 +34,28 @@ generate_growth_df <- function(
     is.data.frame(norm_df),
     start_season %in% c("Fall", "Spring", "Winter"), 
     end_season %in% c("Fall", "Spring", "Winter"),
-    is.logical(include_unsanctioned_windows)
+    is.logical(include_unsanctioned_windows),
+    check_processed_cdf(processed_cdf)$boolean
   )
-  check_cdf_long(prepped_cdf)
-  
+
   #generate a scaffold of students/terms/windows
   f2s <- student_scaffold(processed_cdf, 'Fall', 'Spring', 0)
   f2w <- student_scaffold(processed_cdf, 'Fall', 'Winter', 0)
   w2s <- student_scaffold(processed_cdf, 'Winter', 'Spring', 0)  
   s2s <- student_scaffold(processed_cdf, 'Spring', 'Spring', 1)
-
   scaffolds <- rbind(f2s, f2w, w2s, s2s)
   
-  #match to cdf to get scores
+  #extract scores from long cdf for start and end data
+  start_data <- scores_by_testid(scaffolds$start_testid, processed_cdf, 'start')
+  end_data <- scores_by_testid(scaffolds$end_testid, processed_cdf, 'end')
+  
+  with_scores <- cbind(scaffolds, start_data, end_data)
+  
+  growth_dfs <- list(
+    headline=with_scores
+    #TODO: long goal df here
+  )
+  return(growth_dfs)
 }
 
 
@@ -83,7 +88,7 @@ student_scaffold <- function(
   )
   
   #make a simplified df
-  cols <- c("studentid", "testid", "measurementscale",
+  cols <- c("studentid", "measurementscale", "testid", 
     "map_year_academic", "fallwinterspring", "grade", "grade_level_season", "schoolname"           
   )
   simple <- processed_cdf[ ,cols]
@@ -98,8 +103,8 @@ student_scaffold <- function(
   #namespace stuff
     #normally I avoid reference by index number since, uh, inputs change, but
     #we are hard coding the columns above, so it is OK.
-  start_prefixes <- c(rep("",3), rep("start_", 6))
-  end_prefixes <- c(rep("",3), rep("end_", 6))
+  start_prefixes <- c(rep("",2), rep("start_", 7))
+  end_prefixes <- c(rep("",2), rep("end_", 7))
 
   names(start) <- paste0(start_prefixes, names(start))
   names(end) <- paste0(end_prefixes, names(end))
@@ -116,39 +121,80 @@ student_scaffold <- function(
   
   #a dplyr inner join will return the *matching* rows
   matched_rows <- dplyr::inner_join(
-    x=start, y=end[, c(4:9)], by=c("matching_end_hash" = "end_hash")
+    x=start, y=end[, c(3:9)], by=c("matching_end_hash" = "end_hash")
   )
   
   #dplyr returns a barfy order. reorder.                     
-  col_order <- c("studentid","testid", "measurementscale",      #constants
-    "start_map_year_academic","start_fallwinterspring",         #start cols
-    "start_grade", "start_grade_level_season", "start_schoolname",
-    "end_map_year_academic", "end_fallwinterspring",            #end cols
-    "end_grade", "end_grade_level_season", "end_schoolname",
-    "start_hash", "matching_end_hash"                           #hashes
+  col_order <- c("studentid", "measurementscale",       #constants
+    "start_testid", "start_map_year_academic",          #start cols
+    "start_fallwinterspring", "start_grade", 
+    "start_grade_level_season", "start_schoolname",
+    "end_testid", "end_map_year_academic",              #end cols
+    "end_fallwinterspring", "end_grade", 
+    "end_grade_level_season", "end_schoolname",
+    "start_hash", "matching_end_hash"                   #hashes
   )
-  matched_df <- matched_rows[, col_order]
-  matched_df$match_status <- 'start and end'
+  matched_df <- matched_rows[ ,col_order]
+  matched_df$match_status <- rep('start and end', nrow(matched_df))
+  matched_df$complete_obsv <- rep(TRUE, nrow(matched_df))
 
   #what rows are ONLY found in the start df?
   only_start <- anti_join(
-    x=start, y=matched_df, by=c("start_hash"="start_hash")
+    x=start, y=matched_df, by=c("start_hash" = "start_hash")
   )
   only_start$match_status <- rep('only start', nrow(only_start))
+  only_start$complete_obsv <- rep(FALSE, nrow(only_start))
   
   #what rows are ONLY found in the end df?
   only_end <- anti_join(
     x=end, y=matched_df, by=c("end_hash"="matching_end_hash")
   )
   only_end$match_status <- rep('only end', nrow(only_end))
-
+  only_end$complete_obsv <- rep(FALSE, nrow(only_end))
+  
   #build the df to return
   final <- rbind_all(list(matched_df, only_start, only_end))
   
   #discard some helpers
   final <- final[ ,!names(final) %in% c('start_hash', 'end_hash', 'matching_end_hash')]
   
-  final$growth_window <- paste0(start_season, 'to', end_season)
+  final$growth_window <- paste0(start_season, ' to ', end_season)
   
-  return(final)
+  #reorder
+  final <- final[ ,names(final)[c(1:2, 17:15, 3:14)]]
+  return(as.data.frame(final))
+}
+
+
+
+#' @title scores_by_testid
+#' 
+#' @description helper function for \code{generate_growth_df}. given a test id, 
+#' returns df with all the scores.
+#' 
+#' @param testid
+#' @param processed_cdf
+#' @param start_or_end
+#' 
+#' 
+#' 
+#' @return one row data frame.
+
+scores_by_testid <- function(testid, processed_cdf, start_or_end) {
+  #input validation
+  assert_that(
+    #testids should all an integer
+    #testid %% 1 == 0,
+    is.data.frame(processed_cdf),
+    check_processed_cdf(processed_cdf)$boolean,
+    start_or_end %in% c('start', 'end')
+  )
+
+  matched <- left_join(as.data.frame(testid), processed_cdf, by="testid")
+  
+  matching_slim <- matched[ , c(names(matched)[c(7:14, 19:21, 62:64)])]
+  #prefix it
+  names(matching_slim) <- paste0(start_or_end, '_', names(matching_slim))
+  
+  return(matching_slim)  
 }
--- tests/testthat/test_growth_df_prep.R
+++ tests/testthat/test_growth_df_prep.R
@@ -23,8 +23,8 @@ test_that("prep_cdf_long correctly preps sample data", {
   expect_equal(nrow(f2s_scaffold), 4179)
   expect_equal(nrow(s2s_scaffold), 6466)
   
-  expect_equal(sum(as.numeric(f2s_scaffold$testid)), 510850865583)
-  expect_equal(sum(as.numeric(s2s_scaffold$testid)), 790417103624)
+  expect_equal(sum(as.numeric(f2s_scaffold$start_testid), na.rm=TRUE), 267221268358)
+  expect_equal(sum(as.numeric(f2s_scaffold$end_testid), na.rm=TRUE), 510850832216)
   
   f2s_match_counts <- table(f2s_scaffold$match_status)
   s2s_match_counts <- table(s2s_scaffold$match_status)
@@ -37,3 +37,33 @@ test_that("prep_cdf_long correctly preps sample data", {
   expect_equal(s2s_match_counts[['start and end']], 1892)
   
 })
+
+
+
+test_that("scores_by_testid correctly looks up test events", {
+  
+  ex_lookup <- scores_by_testid(processed_cdf$testid, processed_cdf, 'start')
+  expect_equal(nrow(ex_lookup), 8551)
+  expect_equal(
+    c('start_growthmeasureyn','start_testtype','start_testname',
+      'start_teststartdate','start_testdurationminutes','start_testritscore',
+      'start_teststandarderror','start_testpercentile','start_rittoreadingscore',
+      'start_rittoreadingmin','start_rittoreadingmax','start_teststarttime',
+      'start_percentcorrect','start_projectedproficiency'),
+    names(ex_lookup)
+  )
+  expect_equal(sum(ex_lookup$start_percentcorrect), 435341)
+  expect_equal(sum(ex_lookup$start_testritscore), 1871739)
+})
+
+
+
+test_that("generate_growth_df builds scaffold and finds growth scores", {
+  
+  growth_df <- generate_growth_dfs(processed_cdf)$headline
+  expect_equal(nrow(growth_df), 17010)
+  expect_equal(sum(as.numeric(growth_df$start_testritscore), na.rm=TRUE), 2344192)
+  expect_equal(sum(as.numeric(growth_df$end_testritscore), na.rm=TRUE), 3239190)
+  
+})
+
