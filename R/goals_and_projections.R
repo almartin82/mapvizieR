046a6882381dc6f4a2f4633df7d64c62cccbf7a0
Chris Haid <chaid@kippchicago.org>
2015-02-02 15:50:45 -0600

    Fun stuff!  created add_accelerated_growth() which takes a mapvizier obejct and a accelerared_growth function as argments. takes the growth_df from the mv object and caclutes accelerated growth (Actually takes what ever the goal function wants form the mv object) based on the goal function.  the goal function must return a list with a goals data frame, join_by_fields, and lost name.  with that list, the add_accelerated_growth fucntion adds the list to a slot in the mv_object.  the slot is given the name in the slot_name variable in hte goals_object. IF the add_to_growth_df paramter is TRUE, we then do a join on the grwoth DF and add (or update) the accelrated growth columns to the growth_df.  There is some clean_up fo the df if there superferlous names columns from the join (those ending in .x and .y).  There are two ensurer contracts to check data returned by the goals_function.  This is goin to be rad.

--- R/goals_and_projections.R
+++ R/goals_and_projections.R
@@ -18,7 +18,7 @@
 # one to n for custome goals 
 
 # create data.frame suitable for saving to a 
-goal_kipp_tiered <- function(mapvizier_object, iterations=1, add_to_growth_df=TRUE){
+goal_kipp_tiered <- function(mapvizier_object, iterations=1){
   
   
   x<-ensure_is_mapvizieR(mapvizier_object)
@@ -47,6 +47,7 @@ goal_kipp_tiered <- function(mapvizier_object, iterations=1, add_to_growth_df=TR
            iter=iter+1) %>%
     dplyr::select(studentid, 
                   start_testid, 
+                  end_testid,
                   start_fallwinterspring,
                   end_fallwinterspring, 
                   accel_growth, 
@@ -69,15 +70,80 @@ goal_kipp_tiered <- function(mapvizier_object, iterations=1, add_to_growth_df=TR
     slot_name = "kipp_tiered_goals"
   )
 }    
-  add_accelerated_growth(mv_object, goal_function, goal_function_args){
+
+
+add_accelerated_growth <- function(mapvizier_object, 
+                         goal_function=goal_kipp_tiered, 
+                         goal_function_args=list(iterations=1),
+                         updated_growth_df=FALSE){
     # this should be run in the mapvizier method after the mapvizier class
     # is assigned.  That way it can be used in the constructor method or
     # outside of it for adding new growth to the 
-   goals_obj<-do.call(goal_function, goal_function_args)
    
    
+   goal_function_args$mapvizier_object<-ensure_is_mapvizieR(mapvizier_object)
+   
+   goals_obj<-do.call(goal_function, goal_function_args) %>%
+     ensure_goals_obj
+   
+   #
+   mapvizier_object$goals[[goals_obj$slot_name]]<-goals_obj
+   
+   if(updated_growth_df){
+     new_growth_df<-
+       mapvizier_object$growth_df %>%
+       inner_join(goals_obj$goals,
+                 by=goals_obj$join_by_fields)
+      
+     # clean_up from join
+     # set accel.growth.y= accel.growth 
+     if("accel_growth.y" %in% names(new_growth_df)) {
+       new_growth_df <- new_growth_df %>%
+         mutate(accel_growth.x=accel_growth.y,
+                met_accel_growth.x=met_accel_growth.y) %>%
+         select(accel_growth=accel_growth.x,
+                met_accel_growth=met_accel_growth.x,
+                -accel_growth.y,
+                -met_accel_growth.y) 
+     }
+       
+     #remove .x from names
+     names(new_growth_df)<-gsub("\\.x","", names(new_growth_df))
+     
+     #eliminate .y colums
+     return_cols<-
+       names(new_growth_df)[!grepl("\\.y",names(new_growth_df))]
+     
+     new_growth_df <- new_growth_df[,return_cols] %>%
+       select(-iter) %>%
+       as.data.frame
+     
+     mapvizier_object$growth_df<-new_growth_df
+   }
+   
+   #return
+   mapvizier_object %>% ensure_is_mapvizieR
   }
   
     
+
+
+# ensures ####
+ensure_goals_obj<- ensures_that(+ensure_goals_names,
+  all(
+    c("accel_growth",  "met_accel_growth") %in% names(.$goals)) ~
+              paste0("Your goals function's goals data frame ", "
+                     must have accel_growth and met_accel_growth fields.")
+    )
+
+ensure_goals_names<-ensures_that(
+  all(
+    c("goals", "join_by_fields", "slot_name") %in% 
+      names(.)) ~ 
+    paste0("Your goals function must create a list ", 
+                      "with slots for 'goals', 'join_by_fields', ",  
+                      "and 'slot_name'.")
+  )
+
 
 
