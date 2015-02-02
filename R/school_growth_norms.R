a9e85fb01f70b0ba4839584f9e3e51c667232599
almartin82 <almartin@gmail.com>
2015-02-02 15:28:34 -0500

    silly typo in growth_norms object

--- R/school_growth_norms.R
+++ R/school_growth_norms.R
@@ -16,4 +16,4 @@
 #' \item{sd_of_expectation}{sd_of_expectation}
 #' }
 #' @source http://legacysupport.nwea.org/sites/www.nwea.org/files/resources/2012%20RIT%20Scale%20School%20Norms%20User's%20Guide.pdf
-"sch_growth_norms_2012)"
+"sch_growth_norms_2012"
