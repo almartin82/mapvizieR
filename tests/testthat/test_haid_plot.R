f37139ce6fd9141406f37ff3b281c37e274eda23
almartin82 <almartin@gmail.com>
2015-05-06 17:45:34 -0400

    need coverage of missing testquartile 1, start

--- tests/testthat/test_haid_plot.R
+++ tests/testthat/test_haid_plot.R
@@ -152,6 +152,41 @@ test_that("missing END quartile example", {
 })
 
 
+test_that("missing q1 START example", {
+  special_studentids <- c(
+    "F08000002", "F08000005", "F08000008", "F08000013", "F08000014", 
+    "F08000021", "F08000023", "F08000029", "F08000030", "F08000036", 
+    "F08000037", "F08000046", "F08000052", "F08000053", "F08000059", 
+    "F08000060", "F08000062", "F08000086", "F08000130", "F08000160", 
+    "F08000213", "F08000214", "F08000216", "F08000217", "F08000221", 
+    "F08000234", "F08000236", "F08000245", "F08000260", "F08000261", 
+    "F08000266", "F08000267", "F08000268", "F08000285", "F08000286", 
+    "F08000287", "F08000288", "F08000289", "F08000293", "F08000294", 
+    "F08000295", "F08000303", "F08000304", "F08000306", "F08000308", 
+    "F08000311", "F08000313", "F08000314", "S08000005", "SF06000007", 
+    "SF06000157", "SF06000387", "SF06000405", "SF06000426", "SF06000566", 
+    "SF06000568", "SF06000573", "SF06000783", "SF06000786", "SF06000787", 
+    "SF06001049", "SF06001226", "SF06001375", "SF06001387", "SF07001694", 
+    "SF07001876", "SF07002061", "SS07001540", "SS07001546"
+  )
+    
+  quart_haid <- haid_plot(
+    mapvizieR_obj = mapviz,
+    studentids = special_studentids,
+    measurementscale = 'Mathematics',
+    start_fws = 'Fall',
+    start_academic_year = 2013,
+    end_fws = 'Spring',
+    end_academic_year = 2013
+  )
+  
+  expect_is(quart_haid, 'gg')
+  expect_is(quart_haid, 'ggplot')
+  
+  expect_equal(length(quart_haid), 9)
+})
+
+
 test_that("fuzz test haid_plot", {
   results <- fuzz_test_plot(
     'haid_plot', 
