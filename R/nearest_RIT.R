49f32451bf73e0aea6f334cc75879d14c7733eaa
almartin82 <almartin@gmail.com>
2015-06-03 17:35:39 -0400

    stub of function for james

---
+++ R/nearest_RIT.R
@@ -0,0 +1,26 @@
+#' @title find nearest RIT score for a student, by date
+#' 
+#' @description write a description here
+#' 
+#' @param mapvizieR_obj mapvizieR object
+#' @param studentid target student
+#' @param measurementscale target subject
+#' @param target_date     write a description here
+
+#' @export
+
+
+nearest_rit <- function(  
+  mapvizieR_obj,
+  studentid,
+  measurementscale,
+  target_date
+) {
+  #write some code here
+  
+  #pull out the cdf
+  
+  #find the matchings rows
+  
+  #return something sensible
+}
---
+++ tests/testthat/test_nearest_RIT.R
@@ -0,0 +1,15 @@
+context("nearest_rit tests")
+
+#make sure that constants used below exist
+testing_constants()
+
+
+test_that("nearest_rit does expected stuff ", {
+
+  #psuedocode
+  #samp_val <- nearest_rit(param, param, param)
+  #expect_equal(samp_val, 220)
+  
+  expect_equal(1+1,  2)
+
+})
