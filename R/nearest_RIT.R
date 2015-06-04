59d6ce283d33c37e1f34a10f098d82cead7aa2af
kimjam <jamesykim10@gmail.com>
2015-06-04 10:09:18 -0400

    function done for nearest_RIT.R and tests pass in test_nearest_RIT.R

--- R/nearest_RIT.R
+++ R/nearest_RIT.R
@@ -1,26 +1,43 @@
 #' @title find nearest RIT score for a student, by date
 #' 
-#' @description write a description here
+#' @description Given studentid, measurementscale, and a target_date, the function will return the closest RIT score. 
 #' 
 #' @param mapvizieR_obj mapvizieR object
-#' @param studentid target student
-#' @param measurementscale target subject
-#' @param target_date     write a description here
-
+#' @param sid target studentid
+#' @param measurescale target subject
+#' @param target_date date of interest, %Y-%m-%d format
+#' @param foward default is TRUE, set to FALSE if only scores before target_date should be chosen
+#' 
 #' @export
 
 
-nearest_rit <- function(  
+nearest_rit <- function(
   mapvizieR_obj,
-  studentid,
-  measurementscale,
-  target_date
+  sid,
+  measurescale,
+  target_date,
+  forward=TRUE
 ) {
-  #write some code here
+
+  # pull out the cdf
+  cdf <- mapvizieR_obj[['cdf']]
   
-  #pull out the cdf
+  # find the matchings rows
+  if (require(dplyr)) {
+    student <- cdf %>% dplyr::filter(studentid == sid,
+                                     measurementscale == measurescale)
+  } else {
+    student <- subset(cdf, studentid == sid & measurementscale == measurescale)
+  }
+
+  # filter out rows if forward = FALSE
+  if (!forward) {
+    student <- student[student$teststartdate <= as.Date(target_date),]
+  }
+
+  # find closest date and return rit score on that day
+  diff <- as.numeric(as.Date(target_date) - student$teststartdate)
   
-  #find the matchings rows
+  return(student$testritscore[match(min(abs(diff)),abs(diff))])
   
-  #return something sensible
 }
--- tests/testthat/test_nearest_RIT.R
+++ tests/testthat/test_nearest_RIT.R
@@ -1,15 +1,34 @@
 context("nearest_rit tests")
 
 #make sure that constants used below exist
-testing_constants()
+# testing_constants()
 
 
 test_that("nearest_rit does expected stuff ", {
 
-  #psuedocode
-  #samp_val <- nearest_rit(param, param, param)
-  #expect_equal(samp_val, 220)
+  # test that it works to find closest
+  samp_val <- nearest_rit(mapviz,'F08000002','Mathematics','2013-10-20')
+  expect_equal(samp_val, 209)
   
-  expect_equal(1+1,  2)
-
+  samp_val <- nearest_rit(mapviz,'F08000002','Reading','2013-10-20')
+  expect_equal(samp_val, 217)
+  
+  samp_val <- nearest_rit(mapviz,'F08000002','Language Usage','2013-10-20')
+  expect_equal(samp_val, 215)
+  
+  samp_val <- nearest_rit(mapviz,'F08000006','General Science','2013-10-20')
+  expect_equal(samp_val, 194)
+  
+  # test that forward parameter does its job
+  samp_val <- nearest_rit(mapviz,'F08000002','Mathematics','2013-11-30',forward=FALSE)
+  expect_equal(samp_val, 209)
+  
+  samp_val <- nearest_rit(mapviz,'F08000002','Reading','2013-11-30',forward=FALSE)
+  expect_equal(samp_val, 217)
+  
+  samp_val <- nearest_rit(mapviz,'F08000002','Language Usage','2013-11-30',forward=FALSE)
+  expect_equal(samp_val, 215)
+  
+  samp_val <- nearest_rit(mapviz,'F08000006','General Science','2013-11-30',forward=FALSE)
+  expect_equal(samp_val, 194)
 })
