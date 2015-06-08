08d5dff5867ecc045cf98c0af77c31070387cb63
kimjam <jamesykim10@gmail.com>
2015-06-08 11:47:32 -0400

    trying to improve coverage for estimate_RIT and nearest_RIT

--- tests/testthat/test_estimate_RIT.R
+++ tests/testthat/test_estimate_RIT.R
@@ -1,61 +1,76 @@
 context("estimate_rit tests")
 
+# mapviz2 in global environment defined:
+# mapviz2 <- mapviz
+# mapviz2[['cdf']] <- filter(mapviz2[['cdf']],studentid=='F08000002',measurementscale=='Mathematics')[1,]
+testing_constants()
+
 test_that("estimate_rit does expected stuff ", {
 
   # test closest
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2013-10-20',method='closest')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-10-20',method='closest')
   expect_equal(samp_val, 209)
   
   # test that forward parameter works
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2013-11-30',method='closest',forward=FALSE)
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-11-30',method='closest',forward=FALSE)
   expect_equal(samp_val, 209)
   
   # test lm
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2013-10-20',method='lm')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-10-20',method='lm')
   expect_true(is.numeric(samp_val))
   
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2014-9-20',method='lm')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-9-20',method='lm')
   expect_true(is.numeric(samp_val))
   
   # test interpolate
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2013-10-20',method='interpolate')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-10-20',method='interpolate')
+  expect_true(is.numeric(samp_val))
+  
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-9-12',method='interpolate')
   expect_true(is.numeric(samp_val))
   
   # target_date is a test date
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2014-03-16',method='closest')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-03-16',method='closest')
   expect_equal(samp_val,219)
   
   # check when student hasn't taken a test in given measurementscale
-  samp_val <- estimate_rit(mapviz,'F08000002','General Science','2013-10-20',method='closest')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='General Science',target_date='2013-10-20',method='closest')
   expect_true(is.na(samp_val))
   
-  samp_val <- estimate_rit(mapviz,'F08000003','General Science','2013-10-20',method='closest')
+  samp_val <- estimate_rit(mapviz,studentid='F08000003',measurementscale='General Science',target_date='2013-10-20',method='closest')
   expect_true(is.na(samp_val))
   
   # target_date before first test event
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2013-1-10',method='interpolate')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-1-10',method='interpolate')
   expect_true(is.na(samp_val))
   
   # target_date after last test event
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2014-5-16',method='interpolate')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-5-16',method='interpolate')
   expect_true(is.na(samp_val))
   
   # test that error is returned
-  expect_error(estimate_rit(mapviz,'F08000002','Mathematics','2013-10-20'))
+  expect_error(estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-10-20'))
   
-  expect_error(estimate_rit(mapviz,'F08000002','Mathematics','2013-10-20','logistic'))
+  expect_error(estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-10-20',method='logistic'))
   
-  expect_error(estimate_rit(mapviz,'abcdefg','Mathematics','2013-10-20','closest'))
+  expect_error(estimate_rit(mapviz,studentid='abcdefg',measurementscale='Mathematics',target_date='2013-10-20',method='closest'))
   
   # test num_days
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2014-9-20','closest')
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-9-20',method='closest')
   expect_true(is.na(samp_val))
   
   # change num_days to include this date
-  samp_val <- estimate_rit(mapviz,'F08000002','Mathematics','2014-9-20','closest',num_days=200)
+  samp_val <- estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-9-20',method='closest',num_days=200)
   expect_equal(samp_val,219)
   
   # measurement scale not given / spelled wrong
-  expect_error(estimate_rit(mapviz,'F08000002','2013-10-20'))
-  expect_error(estimate_rit(mapviz,'F08000002','Mathatacsa','2013-9-20'))
+  expect_error(estimate_rit(mapviz,studentid='F08000002',target_date='2013-10-20'))
+  expect_error(estimate_rit(mapviz,studentid='F08000002',measurementscale='Mathatacsa',target_date='2013-9-20'))
+  
+  # only one test event for measurement scale
+  samp_val <- estimate_rit(mapviz2,studentid='F08000002',measurementscale='Mathematics',target_date='2013-9-20',method='closest')
+  expect_equal(samp_val,209)
+  
+  samp_val <- estimate_rit(mapviz2,studentid='F08000002',measurementscale='Mathematics',target_date='2015-9-20',method='closest')
+  expect_true(is.na(samp_val))
 })
--- tests/testthat/test_nearest_RIT.R
+++ tests/testthat/test_nearest_RIT.R
@@ -3,40 +3,40 @@ context("nearest_rit tests")
 test_that("nearest_rit does expected stuff ", {
 
   # test that it works to find closest
-  samp_val <- nearest_rit(mapviz,'F08000002','Mathematics','2013-10-20')
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-10-20')
   expect_equal(samp_val, 209)
   
-  samp_val <- nearest_rit(mapviz,'F08000002','Reading','2013-10-20')
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Reading',target_date='2013-10-20')
   expect_equal(samp_val, 217)
   
-  samp_val <- nearest_rit(mapviz,'F08000002','Language Usage','2013-10-20')
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Language Usage',target_date='2013-10-20')
   expect_equal(samp_val, 215)
   
-  samp_val <- nearest_rit(mapviz,'F08000006','General Science','2013-10-20')
+  samp_val <- nearest_rit(mapviz,studentid='F08000006',measurementscale='General Science',target_date='2013-10-20')
   expect_equal(samp_val, 194)
   
   # test that forward parameter does its job
-  samp_val <- nearest_rit(mapviz,'F08000002','Mathematics','2013-11-30',forward=FALSE)
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2013-11-30',forward=FALSE)
   expect_equal(samp_val, 209)
   
-  samp_val <- nearest_rit(mapviz,'F08000002','Reading','2013-11-30',forward=FALSE)
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Reading',target_date='2013-11-30',forward=FALSE)
   expect_equal(samp_val, 217)
   
-  samp_val <- nearest_rit(mapviz,'F08000002','Language Usage','2013-11-30',forward=FALSE)
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Language Usage',target_date='2013-11-30',forward=FALSE)
   expect_equal(samp_val, 215)
   
-  samp_val <- nearest_rit(mapviz,'F08000006','General Science','2013-11-30',forward=FALSE)
+  samp_val <- nearest_rit(mapviz,studentid='F08000006',measurementscale='General Science',target_date='2013-11-30',forward=FALSE)
   expect_equal(samp_val, 194)
   
   # test num_days
-  samp_val <- nearest_rit(mapviz,'F08000002','Mathematics','2014-9-20')
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-9-20')
   expect_true(is.na(samp_val))
   
-  samp_val <- nearest_rit(mapviz,'F08000002','Mathematics','2014-9-20',num_days=200)
+  samp_val <- nearest_rit(mapviz,studentid='F08000002',measurementscale='Mathematics',target_date='2014-9-20',num_days=200)
   expect_equal(samp_val,219)
   
   # test errors
-  expect_error(nearest_rit(mapviz,'F08000002','2013-10-20'))
-  expect_error(nearest_rit(mapviz,'F08000002','Mathatacsa','2013-9-20'))
-  expect_error(nearest_rit(mapviz,'abcdefg','Mathematics','2013-10-20'))
+  expect_error(nearest_rit(mapviz,studentid='F08000002',target_date='2013-10-20'))
+  expect_error(nearest_rit(mapviz,studentid='F08000002',measurementscale='Mathatacsa',target_date='2013-9-20'))
+  expect_error(nearest_rit(mapviz,studentid='abcdefg',measurementscale='Mathematics',target_date='2013-10-20'))
 })
