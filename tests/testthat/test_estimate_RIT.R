fcf8a7b662a4b45b0d90c8f01d5a815c7ebe7dcd
almartin82 <almartin@gmail.com>
2015-06-12 10:21:29 -0400

    more test coverage on estimate RIT

--- tests/testthat/test_estimate_RIT.R
+++ tests/testthat/test_estimate_RIT.R
@@ -3,7 +3,7 @@ context("estimate_rit tests")
 # mapviz2 in global environment defined:
 testing_constants()
 
-test_that("estimate_rit does expected stuff ", {
+test_that("estimate_rit normal behavior", {
 
   # test closest
   samp_val <- estimate_rit(mapviz, studentid = 'F08000002', 
@@ -69,23 +69,46 @@ test_that("estimate_rit does expected stuff ", {
                            target_date = '2014-5-16',
                            method = 'interpolate')
   expect_true(is.na(samp_val))
+})
+    
+
+test_that("estimate_rit error conditions", {
   
-  # test that error is returned
+  #no method
   expect_error(
     estimate_rit(mapviz, studentid = 'F08000002', measurementscale = 'Mathematics', 
-                 target_date = '2013-10-20')
+                 target_date = '2013-10-20'), "method not given"
   )
   
+  #bad method
   expect_error(
     estimate_rit(mapviz, studentid = 'F08000002', measurementscale = 'Mathematics',
-                 target_date = '2013-10-20', method = 'logistic')
+                 target_date = '2013-10-20', method = 'logistic'), "method not available"
   )
   
+  #bad student
   expect_error(
     estimate_rit(mapviz, studentid = 'abcdefg', measurementscale = 'Mathematics', 
-                 target_date = '2013-10-20', method = 'closest')
+                 target_date = '2013-10-20', method = 'closest'), "studentid not in mapvizieR cdf object"
   )
   
+  #no measurementscale
+  expect_error(
+    estimate_rit(mapviz, studentid = 'F08000002', target_date = '2013-10-20', method = 'closest'), 
+    "measurementscale not given"
+  )
+
+  #bad measurementscale
+  expect_error(
+    estimate_rit(mapviz, studentid = 'F08000002', measurementscale = 'Science', 
+    target_date = '2013-10-20', method = 'closest'), "invalid measurementscale"
+  )
+
+})
+
+
+test_that("advanced estimate_rit options", {
+
   # test num_days
   samp_val <- estimate_rit(mapviz, studentid = 'F08000002', 
                            measurementscale = 'Mathematics', 
@@ -96,12 +119,6 @@ test_that("estimate_rit does expected stuff ", {
   samp_val <- estimate_rit(mapviz, studentid = 'F08000002',measurementscale = 'Mathematics',target_date = '2014-9-20',method = 'closest',num_days = 200)
   expect_equal(samp_val, 219, tolerance = .01)
   
-  # measurement scale not given / spelled wrong
-  expect_error(estimate_rit(mapviz, studentid = 'F08000002', target_date = '2013-10-20'))
-  expect_error(
-    estimate_rit(mapviz,studentid = 'F08000002', measurementscale = 'Mathatacsa', 
-                 target_date = '2013-9-20')
-    )
   
   # only one test event for measurement scale
   # filtered mapviz object to test for errors
