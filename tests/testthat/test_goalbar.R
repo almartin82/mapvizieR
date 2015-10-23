d69d2857722b796753793eb85351d86f45999239
Chris Haid <chaid@kippchicago.org>
2015-10-23 14:33:31 -0500

    Fixed some tests

--- tests/testthat/test_goalbar.R
+++ tests/testthat/test_goalbar.R
@@ -19,18 +19,18 @@ test_that("goalbar produces proper plot with a grade level of kids", {
   p_build <- ggplot_build(p)
   expect_true(is.ggplot(p))
   expect_equal(nrow(p_build$data[[1]]), 4)
-  expect_equal(sum(p_build$data[[1]][, 6]), 154, tolerance=.001)  
+  expect_equal(sum(p_build$data[[1]][, 6]), 155, tolerance=.001)  
   expect_equal(ncol(p_build$data[[2]]), 6)
-  expect_equal(sum(p_build$data[[2]][, 3]), 200.5, tolerance=.001)
+  expect_equal(sum(p_build$data[[2]][, 3]), 201.5, tolerance=.001)
   
   p <- goalbar(mapviz, studentids_normal_use, 'Mathematics', 'Fall', 2013,
          'Spring', 2013, complete_obsv=TRUE)
   p_build <- ggplot_build(p)
   expect_true(is.ggplot(p))
   expect_equal(nrow(p_build$data[[1]]), 4)
-  expect_equal(sum(p_build$data[[1]][, 6]), 154, tolerance=.001)  
+  expect_equal(sum(p_build$data[[1]][, 6]), 155, tolerance=.001)  
   expect_equal(ncol(p_build$data[[2]]), 6)
-  expect_equal(sum(p_build$data[[2]][, 3]), 200.5, tolerance=.001)
+  expect_equal(sum(p_build$data[[2]][, 3]), 201.5, tolerance=.001)
 
 })
 
@@ -43,7 +43,7 @@ test_that("goalbar works with ontrack params",{
   expect_true(is.ggplot(p))
   
   p_build <- ggplot_build(p)
-  expect_equal(sum(p_build$data[[2]][, 3]), 200.5, tolerance=.001)
+  expect_equal(sum(p_build$data[[2]][, 3]), 201.5, tolerance=.001)
   
 })
 
