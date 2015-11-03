34942204dee89ac19bf04f60c9cde9ea7d48a1b5
almartin82 <almartin@gmail.com>
2015-11-03 09:34:43 -0500

    more localization tests

--- tests/testthat/test_localization.R
+++ tests/testthat/test_localization.R
@@ -5,12 +5,17 @@ testing_constants()
 
 test_that("localization function works as expected", {
 
-  knj <- localize("Newark")
-  def <- localize("Toronto")
+  knj <- localize("Newark", verbose = TRUE)
+  def <- localize("Toronto", verbose = TRUE)
   
   expect_is(knj, 'list')
   expect_is(def, 'list')
   
   expect_equal(def$act_cuts, c(11, 16, 18, 22, 25, 29))
   
+  expect_output(localize("Newark", verbose = TRUE), "Localized 4 variables")
+  expect_output(
+    localize("Toronto", verbose = TRUE), 
+    "Your localization choice did not match"
+  )
 })
