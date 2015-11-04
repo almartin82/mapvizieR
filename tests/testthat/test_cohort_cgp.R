91067bbb895c3f2a493aa29897d1b9dcc96f38ab
almartin82 <almartin@gmail.com>
2015-11-04 18:09:58 -0500

    add tests

---
+++ tests/testthat/test_cohort_cgp.R
@@ -0,0 +1,18 @@
+context("cohort cgp history tests")
+
+#make sure that constants used below exist
+testing_constants()
+
+test_that("cohort_cgp_hist_plot should return a plot", {  
+  
+  p <- cohort_cgp_hist_plot(
+    mapvizieR_obj = mapviz,
+    studentids = studentids_normal_use,
+    measurementscale = 'Mathematics',
+    entry_grade_seasons = c(-0.8, 5.2)
+  ) 
+  
+  expect_is(p, 'ggplot')
+  p <- ggplot_build(p)
+  expect_equal(p$data[[2]]$y %>% round(2) %>% sum(), 134.33)
+})
