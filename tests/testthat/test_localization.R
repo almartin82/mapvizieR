a9de3afe1b896ae64e6f461ef7925b979d8ad689
almartin82 <almartin@gmail.com>
2015-11-11 15:27:17 -0500

    trivial test on localization

--- tests/testthat/test_localization.R
+++ tests/testthat/test_localization.R
@@ -13,7 +13,7 @@ test_that("localization function works as expected", {
   
   expect_equal(def$act_cuts, c(11, 16, 18, 22, 25, 29))
   
-  expect_output(localize("Newark", verbose = TRUE), "Localized 4 variables")
+  expect_output(localize("Newark", verbose = TRUE), "Localized 5 variables")
   expect_output(
     localize("Toronto", verbose = TRUE), 
     "Your localization choice did not match"
