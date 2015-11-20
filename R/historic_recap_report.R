028a8f9f165daf534dc6fbaa54406bd8e7e01cf3
almartin82 <almartin@gmail.com>
2015-11-19 21:53:41 -0500

    vignette with template layouts

--- R/historic_recap_report.R
+++ R/historic_recap_report.R
@@ -1,5 +1,24 @@
-
-
+#' Historic Recap Report
+#'
+#' @description looks back across multiple cohorts.  
+#' summarizes growth and and attainment
+#' 
+#' @param mapvizieR_obj conforming mapvizieR object
+#' @param studentids vector of studentids
+#' @param measurementscale target subject
+#' @param target_percentile integer, what is the 'goal' percentile to show
+#' progress against?
+#' @param first_and_spring_only logical, should we drop fall and winter 
+#' scores?
+#' @param entry_grade_seasons numeric vector, what are 'entry' grades to the school?
+#' @param small_n_cutoff numeric, drop observations that are smaller than X% of the
+#' cohort maximum.
+#' @param min_cohort_size integer, filter out cohorts with less than this many students
+#' in them.
+#' @param title_text report title
+#'
+#' @return grob, with plots arranged
+#' @export
 
 historic_recap_report <- function(
   mapvizieR_obj,
@@ -45,4 +64,40 @@ historic_recap_report <- function(
     h_var(title_text, 36), t,
     nrow = 2, heights = c(1, 17)
   )
+}
+
+
+historic_recap_report_detail <- function(
+    mapvizieR_obj, 
+    studentids, 
+    measurementscale,
+    subgroup_cols = c('starting_quartile'),
+    pretty_names = c('Starting Quartile'),
+    magic_subgroups = FALSE,
+    start_fws,
+    start_year_offset,
+    end_fws,
+    end_academic_year,
+    start_fws_prefer = NA
+) {
+  
+  p1 <- quealy_subgroups(
+    mapvizieR_obj = mapvizieR_obj, 
+    studentids = studentids, 
+    measurementscale = measurementscale,
+    subgroup_cols = subgroup_cols,
+    pretty_names = pretty_names,
+    magic_subgroups = magic_subgroups,
+    start_fws = start_fws,
+    start_year_offset = start_year_offset,
+    end_fws = end_fws,
+    end_academic_year = end_academic_year,
+    start_fws_prefer = start_fws_prefer
+  )
+  p2 <- textGrob('quealy')
+  p3 <- textGrob('cgp table')
+  p4 <- textGrob('schambach')
+  p5 <- textGrob('becca')
+
+  template_06(p1, p2, p3, p4, p5)
 }
--- R/templates.R
+++ R/templates.R
@@ -118,3 +118,32 @@ template_05 <- function(p01, p02) {
     p01 %>% tg(), p02 %>% tg(), nrow = 2    
   )
 }
+
+
+
+#' template 6
+#' 
+#' @description col 1: (1 x 1), col 2: 1, col 3: (1 x 1)
+#'
+#' @param p01 plot or grob
+#' @param p02 plot or grob
+#' @param p03 plot or grob
+#' @param p04 plot or grob
+#' @param p05 plot or grob
+#'
+#' @return grob, output of arrangeGrob
+#' @export
+
+template_06 <- function(p01, p02, p03, p04, p05) {
+  grid.newpage()
+  
+  arrangeGrob(
+    grobs = list(
+      p01 %>% tg(), p02 %>% tg(), p03 %>% tg(), p04 %>% tg(), p05 %>% tg()
+    ), 
+    layout_matrix = cbind(
+      c(1, 2, 2, 2, 2, 2, 2, 2, 2), 
+      c(3, 3, 3, 4, 4, 4, 5, 5, 5)),
+    ncol = 2, widths = c(2, 1)
+  )
+}
