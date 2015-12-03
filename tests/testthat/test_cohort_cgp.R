398c9e33272e8beea413653e3ec85dd0a651d3e2
almartin82 <almartin@gmail.com>
2015-12-03 10:00:07 -0500

    more tests on cohort_cgp

--- tests/testthat/test_cohort_cgp.R
+++ tests/testthat/test_cohort_cgp.R
@@ -27,3 +27,42 @@ test_that("cohort_cgp_hist_plot should return a plot", {
   
 })
 
+
+test_that("cohort_cgp_hist_plot parameter args", {  
+  
+  p <- cohort_cgp_hist_plot(
+    mapvizieR_obj = mapviz,
+    studentids = studentids_normal_use,
+    measurementscale = 'Mathematics',
+    first_and_spring_only = FALSE,
+    entry_grade_seasons = c(-0.8, 5.2),
+    no_labs = TRUE
+  ) 
+  
+  expect_is(p, 'ggplot')
+  p <- ggplot_build(p)
+  expect_equal(p$data[[2]]$y %>% round(2) %>% sum(na.rm = TRUE), 90.07)
+  
+})
+
+
+test_that("mult_cohort_cgp_hist_plot produces valid plot", {  
+  
+  p_mult <- multi_cohort_cgp_hist_plot(
+    mapvizieR_obj = mapviz,
+    studentids = studentids_ms,
+    measurementscale = 'Mathematics',
+    first_and_spring_only = FALSE,
+    entry_grade_seasons = c(-0.8, 5.2)
+  )
+  
+  expect_is(p_mult, 'ggplot')
+  p_mult <- ggplot_build(p_mult)
+  expect_equal(p_mult$data[[1]]$y %>% round(2) %>% sum(na.rm = TRUE), 874.3)
+  
+})
+
+
+
+
+
