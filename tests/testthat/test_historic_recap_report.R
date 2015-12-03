7ec15a068b44abdf666b47907504ec79c32e5090
almartin82 <almartin@gmail.com>
2015-12-03 11:53:24 -0500

    test fixes

--- tests/testthat/test_historic_recap_report.R
+++ tests/testthat/test_historic_recap_report.R
@@ -28,9 +28,9 @@ test_that("historic recap report produces valid plot", {
   expect_equal(
     p$grobs[[2]][[1]][[2]][[1]][[11]]$children$axis$grobs[[2]]$x,
     structure(
-      c(0.716450216450217, 0.932900432900433, 0.5, 0.326839826839827, 
-        0.283549783549784, 0.0670995670995671), unit = "native", 
-        valid.unit = 4L, class = "unit")
+      c(0.850649350649351, 0.928571428571429, 0.590909090909091, 
+        0.668831168831169, 0.331168831168831, 0.409090909090909, 0.0714285714285713, 
+        0.149350649350649), unit = "native", valid.unit = 4L, class = "unit")
   )
   
 })
