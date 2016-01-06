44b8d10b8dbc602ab0fd567e5ad108b3cbcf9e28
almartin82 <almartin@gmail.com>
2016-01-06 16:11:39 -0500

    changes on summary tests after filtering by matched rows

--- tests/testthat/test_summary_mapvizier.R
+++ tests/testthat/test_summary_mapvizier.R
@@ -1,12 +1,11 @@
 context("mapvizier_summary tests")
 
-test_that("sumamry works as expected", {
+test_that("summary works as expected", {
   ex <- summary(mapviz)
-  expect_equal(ex$end_pct_75th_pctl %>% sum(na.rm = TRUE), 41.56)
-  expect_equal(nrow(ex), 346)
-  expect_equal(sum(ex$cgp, na.rm = TRUE), 7314.03)
+  expect_equal(ex$end_pct_75th_pctl %>% sum(na.rm = TRUE), 37.49)
+  expect_equal(nrow(ex), 149)
+  expect_equal(sum(ex$cgp, na.rm = TRUE), 7344.26)
   
   ex2 <- summary(mapviz, digits = 3)
-  expect_equal(ex2$end_pct_75th_pctl %>% sum(na.rm = TRUE), 41.544)
-  
+  expect_equal(ex2$end_pct_75th_pctl %>% sum(na.rm = TRUE), 37.462)
 })
