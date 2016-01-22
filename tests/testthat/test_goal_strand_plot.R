9d8e727289731fa647af36e4831e7bfe65a4c9cf
Chris Haid <chaid@kippchicago.org>
2016-01-22 13:55:57 -0600

    Updated documentation

--- tests/testthat/test_goal_strand_plot.R
+++ tests/testthat/test_goal_strand_plot.R
@@ -25,3 +25,77 @@ test_that("goal_strand_plot produces proper plot with a grade level of kids", {
   expect_equal(nrow(p_build$plot$data), 372)
 
 })
+
+test_that("Goals strand summary and range works for a given year", {
+  
+  p <- goal_strand_summary_plot(
+    mapviz,
+    studentids_normal_use,
+    measurementscale = "Mathematics", 
+    fws = "Spring", 
+    year = 2013
+  )
+  
+  p_build <- ggplot_build(p)
+  
+  expect_true(is.ggplot(p))
+  expect_equal(nrow(p_build$plot$data), 4)
+  
+})
+
+
+test_that("Goals strand summary and range works for a given cohort", {
+  
+  p <- goal_strand_summary_plot(
+    mapviz,
+    studentids_normal_use,
+    measurementscale = "Mathematics", 
+    fws = c("Fall", "Spring"), 
+    cohort = 2020,
+    spring_is_first = TRUE
+  )
+  
+  p_build <- ggplot_build(p)
+  
+  expect_true(is.ggplot(p))
+  expect_equal(nrow(p_build$plot$data), 12)
+  
+})
+
+test_that("Goals strand summary and range works for a given year", {
+  
+  p <- goal_strand_summary_plot(
+    mapviz,
+    studentids_normal_use,
+    measurementscale = "Mathematics", 
+    fws = "Spring", 
+    year = 2013
+  )
+  
+  p_build <- ggplot_build(p)
+  
+  expect_true(is.ggplot(p))
+  expect_equal(nrow(p_build$plot$data), 4)
+  
+})
+
+
+test_that("Goals strand summary and range works with filter_args", {
+  
+  p <- goal_strand_summary_plot(
+    mapviz,
+    studentids_normal_use,
+    measurementscale = "Mathematics", 
+    fws = c("Fall", "Spring"), 
+    cohort = 2020,
+    spring_is_first = TRUE,
+    filter_args = list("grade == 6", "grepl('Bach', schoolname)")
+  )
+  
+  p_build <- ggplot_build(p)
+  
+  expect_true(is.ggplot(p))
+  expect_equal(nrow(p_build$plot$data), 8)
+  
+})
+
