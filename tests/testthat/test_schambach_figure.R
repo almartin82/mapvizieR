48c23cac8d0415d6b2e02b9f42d79a13efadc073
Andrew Martin <amartin@publicprep.org>
2017-01-31 09:32:38 -0500

    tests on schambach fig

--- tests/testthat/test_schambach_figure.R
+++ tests/testthat/test_schambach_figure.R
@@ -1,17 +1,17 @@
 context("schambach_figure tests")
 
 test_that("schambach_figure should return list of tables", {  
-  figs <- schambach_figure(
-    mapvizieR_obj = mapviz,
-    measurementscale_in = 'Reading',
-    studentids_in = c(paste0('F0800000', 1:9), paste0('F080000', 10:99)),
-    subgroup_cols = c('grade', 'studentgender', 'studentethnicgroup'),
-    pretty_names = c('Grade', 'Gender', 'Ethnicity'),
-    start_fws = 'Fall',
-    start_academic_year = 2013,
-    end_fws = 'Spring',
-    end_academic_year = 2013
-  )
+    figs <- schambach_figure(
+      mapvizieR_obj = mapviz,
+      measurementscale_in = 'Reading',
+      studentids_in = c(paste0('F0800000', 1:9), paste0('F080000', 10:99)),
+      subgroup_cols = c('grade', 'studentgender', 'studentethnicgroup'),
+      pretty_names = c('Grade', 'Gender', 'Ethnicity'),
+      start_fws = 'Fall',
+      start_academic_year = 2013,
+      end_fws = 'Spring',
+      end_academic_year = 2013
+    )
   
   expect_equal(length(figs), 3)
   expect_true("grob" %in% class(figs)) 
