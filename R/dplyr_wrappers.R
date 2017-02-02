11fb83753bb4b2df700b62de9537d9a0d63f8b05
almartin82 <almartin@gmail.com>
2017-02-02 08:46:50 -0500

    more dplyr wrapper work for addl verbs

--- R/dplyr_wrappers.R
+++ R/dplyr_wrappers.R
@@ -35,11 +35,101 @@ group_by_.mapvizieR_data <- function(df, ...) {
   class(df) <- old_classes[!old_classes == 'mapvizieR_data']
   
   #call normal group_by (should go to dplyr)
-  out <- group_by_(df, ...)
+  out <- dplyr::group_by_(df, ...)
   
   #restore class info
   new_classes <- class(out)
-  class(out) <- c(new_classes, old_classes) %>% unique()
+  all_classes <- c(new_classes, old_classes) %>% unique() %>% sort()
+  sorted_classes <- wrapper_class_orderer(all_classes)
+  class(out) <- sorted_classes
+  
+  out
+}
+
+
+
+#' class orderer for dplyr wrappers
+#'
+#' @param all_classes character vector of class names
+#'
+#' @return character vector of classes with anything `mapviz` sorted first
+#' @export
+
+wrapper_class_orderer <- function(all_classes) {
+
+  #force mapviz classes first
+  is_mapviz <- grepl('mapviz', all_classes) 
+  
+  #starting position
+  sort_order <- seq(1:length(all_classes))
+  sort_order <- sort_order + 1000
+  sort_order[is_mapviz] <- sort_order[is_mapviz] - 50
+  
+  sorted_classes <- all_classes[rank(sort_order)]
+  
+  sorted_classes
+}
+
+
+#' ungroup wrapper
+#'
+#' @description wrapper for ungroup that preserves classes of data frames
+#' @param df data.frame
+#' @param ... additional args
+#' @rdname ungroup
+#' 
+#' @return data.frame
+#' @export
+
+ungroup_.mapvizieR_data <- function(df, ...) {
+  
+  #store the incoming class info
+  old_classes <- class(df)
+  
+  #strip mavpvizieR_data class so that method dispatch doesn't lead to 
+  #infinite calls back to group_by_.mapvizieR_data
+  class(df) <- old_classes[!old_classes == 'mapvizieR_data']
+  
+  #call normal group_by (should go to dplyr)
+  out <- dplyr::ungroup(df, ...)
+  
+  #restore class info
+  new_classes <- class(out)
+  all_classes <- c(new_classes, old_classes) %>% unique() %>% sort()
+  sorted_classes <- wrapper_class_orderer(all_classes)
+  class(out) <- sorted_classes
+  
+  out
+}
+
+
+#' select wrapper
+#'
+#' @description wrapper for select that preserves classes of data frames
+#' @param df data.frame
+#' @param ... additional args
+#' @rdname select
+#' 
+#' @return data.frame
+#' @export
+
+select_.mapvizieR_data <- function(df, ...) {
+  
+  #store the incoming class info
+  old_classes <- class(df)
+  
+  #strip mavpvizieR_data class so that method dispatch doesn't lead to 
+  #infinite calls back to group_by_.mapvizieR_data
+  class(df) <- old_classes[!old_classes == 'mapvizieR_data']
+  
+  #call normal group_by (should go to dplyr)
+  out <- dplyr::select_(df, ...)
+  
+  #restore class info
+  new_classes <- class(out)
+  all_classes <- c(new_classes, old_classes) %>% unique() %>% sort()
+  sorted_classes <- wrapper_class_orderer(all_classes)
+  class(out) <- sorted_classes
   
   out
 }
---
+++ tests/testthat/test_dplyr_wrappers.R
@@ -0,0 +1,19 @@
+context("dplyr wrappers work as expected")
+
+group_by_ex <- mapviz$cdf %>%
+  dplyr::group_by(map_year_academic, schoolname, measurementscale)
+
+ungroup_ex <- mapviz$cdf %>%
+  dplyr::ungroup()
+
+select_ex <- mapviz$cdf %>%
+  dplyr::select(termname, studentid, schoolname, measurementscale, discipline, growthmeasureyn)
+
+test_that("group_by preserves class info", {
+  expect_is(group_by_ex, 'mapvizieR_data')
+})
+
+test_that("ungroup preserves class info", {
+  expect_is(ungroup_ex, 'mapvizieR_data')
+})
+
