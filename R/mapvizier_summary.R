e2a38fdbda141f9c67e30c3273ea9b3024094386
Chris Haid <chaid@kippchicago.org>
2019-10-21 06:52:00 -0500

    Same as last commit, but for summary.mapvizieR_cdf

--- R/mapvizier_summary.R
+++ R/mapvizier_summary.R
@@ -173,7 +173,7 @@ summary.mapvizieR_cdf <- function(object, ...) {
   #summary.mapvizieR_cdf requires grouping vars on the cdf
   #process_cdf_long sets them as part of construction of the mv object
   #if there are NO grouping vars, this will set them by default
-  existing_groups <- attr(object, 'vars') %>% as.character()
+  existing_groups <- groups(object) %>% as.character()
   
   if (is.null(existing_groups)) {
     object <- object %>%
